ca83e99556c8404ef155ef054fcfdc4e334a6a02
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-13 05:51:56 -0500

    first commit

---
+++ src/main/java/com/example/dropwizarddemo/DropwizarddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dropwizarddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropwizarddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropwizarddemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dropwizarddemo/DropwizarddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dropwizarddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropwizarddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
